d9da26cbf42f8a3480faf41b64732849e96c9185
= <=>
2020-05-07 15:51:44 +0200

    changes

--- supprimer.php
+++ supprimer.php
@@ -1,9 +1,10 @@
 <?php
-$bdd = new PDO("mysql:host=127.0.0.1;dbname=articles;charset=utf8", "nader", "nader");
+include "conf.php";
+
 if(isset($_GET['id']) AND !empty($_GET['id'])) {
    $suppr_id = htmlspecialchars($_GET['id']);
    $suppr = $bdd->prepare('DELETE FROM articles WHERE id = ?');
    $suppr->execute(array($suppr_id));
-   header('Location: http://127.0.0.1/final/');
+   header('Location: http://127.0.0.1/naderProjet/');
 }
 ?>
